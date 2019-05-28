341e12c16fee7378edced96ab40d8086c337704d
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-05-28 15:41:21 -0700

    readme file added and sign-up form part started

---
+++ index.php
@@ -0,0 +1,11 @@
+
+
+<form method="post">
+    <input type="email" name="email" placeholder="Enter your email">
+    <input type="password" name="password" placeholder="Enter your password">
+    <input type="checkbox" name="stayLoggedIn" value=1 >
+    <input type="submit" name="submit" value="Sign up!" >
+
+</form>
+
+    
---
+++ readme.md
@@ -0,0 +1,9 @@
+This site is called Keeper of Secrets. It shall be
+able to let users keep all their thoughts and secrets.
+The only way for someone to view thier thoughts and secrets
+is by logging in to the account that they made for themselfs.
+
+Techonology:
+
+Front-end: Bootstrap
+Back-end: MYSQL, PHP
