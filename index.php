fe11326f81a529f6423480224682c71862bff5d3
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-05-29 16:43:07 -0700

    we can now switch between Signup and Login instead of having them both be displayed at once

--- index.php
+++ index.php
@@ -146,6 +146,10 @@
             color: white; 
         }
 
+        #loginform {
+            display: none; 
+        }
+
         
 
     </style>
@@ -160,7 +164,7 @@
 
         <div id="error"> <?php  echo $error; ?> </div>
 
-        <form method="post">
+        <form method="post"id="signUpform">
             <fieldset class="form-group">
             <input class="form-control" type="email" name="email" placeholder="Enter your email">
             </fieldset>
@@ -180,15 +184,18 @@
 
             <fieldset class="form-group">
             <input class="form-control" type="hidden" name="signUp" value="1">
+
             </fieldset>
 
             <fieldset class="form-group">
             <input class="btn btn-success" type="submit" name="submit" value="Sign up!" >
             </fieldset>
 
+            <p> <a class="toggleForms"> Log In </a> </p>
+
         </form>
 
-        <form method="post">
+        <form method="post" id="loginform">
         <fieldset class="form-group">
             <input  class="form-control" type="email" name="email" placeholder="Enter your email">
             </fieldset>
@@ -213,6 +220,8 @@
             <input class="btn btn-success" type="submit" name="submit" value="Log In!" >
             </fieldset>
 
+            <p> <a class="toggleForms"> Sign up </a> </p>
+
         </form>
     </div>
 
@@ -223,6 +232,16 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Add jquery that will allow you to switch between login and sign up -->
+    <script type= "text/javascript"> 
+
+    $("#signUpform").toggle(); 
+    $("#loginform").toggle(); 
+    
+
+    </script> 
+
   </body>
 </html>
 
