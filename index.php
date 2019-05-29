595d1d54ae9ca17fc14d20523ce6f8ecbe781dcd
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-05-29 16:20:37 -0700

    Added some stylying and made background image full size of the screen

--- index.php
+++ index.php
@@ -122,6 +122,34 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Keeper of Secrets!</title>
+
+    <style type="text/css">
+
+        .container{
+            text-align: center;
+            width: 400px; 
+            margin-top: 150px; 
+        }
+
+        // making background picture so it is stretched to the full screen 
+
+        html {
+            background: url(background.jpg) no-repeat center center fixed; 
+            -webkit-background-size: cover; 
+            -moz-background-size: cover; 
+            -o-background-size: cover; 
+            background-size: cover; 
+        }
+
+        body{
+            background: none; 
+            color: white; 
+        }
+
+        
+
+    </style>
+
   </head>
   <body>
     
