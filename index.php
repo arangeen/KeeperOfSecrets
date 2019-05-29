5561489e0293709f4b144bc468ce03603399eba8
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-05-29 13:50:27 -0700

    users can now log in to their account if they already created one

--- index.php
+++ index.php
@@ -44,37 +44,66 @@
             $error = "<p> There were error(s) in your input: </p>".$error; 
         }else {
 
-            $query = "SELECT id FROM 'users' WHERE 
-                        email = '".mysqli_real_escape_string($link, $_POST['email']). "' LIMIT 1";
-            // run the query 
-            $result = mysqli_query($link, $query); 
+            if ($_POST['signUp']=='1') {
+                $query = "SELECT id FROM 'users' WHERE 
+                            email = '".mysqli_real_escape_string($link, $_POST['email']). "' LIMIT 1";
+                // run the query 
+                $result = mysqli_query($link, $query); 
 
-            if(mysqli_num_rows($result) > 0) {
-                $error = "That email address is taken."; 
-            } else {
-                $query = "INSERT INTO 'users' ('email' , 'password') VALUES 
-                ( '".mysqli_real_escape_string($link, $_POST['email'])."' , 
-                 '".mysqli_real_escape_string($link, $_POST['password'])."')";
+                if(mysqli_num_rows($result) > 0) {
+                    $error = "That email address is taken."; 
+                } else {
+                    $query = "INSERT INTO 'users' ('email' , 'password') VALUES 
+                    ( '".mysqli_real_escape_string($link, $_POST['email'])."' , 
+                    '".mysqli_real_escape_string($link, $_POST['password'])."')";
 
-                 if(!mysqli_query($link , $query)) {
-                     $error = "<p> Could not sign you up. Please try again later. </p> "; 
-                 }else {
-                    $query = "UPDATE 'users' SET password = 
-                    '".md5(md5(mysqli_insert_id()). $_POST['password'])."'
-                    WHERE id = ".mysqli_insert_id($link)." LIMIT 1"; 
+                    if(!mysqli_query($link , $query)) {
+                        $error = "<p> Could not sign you up. Please try again later. </p> "; 
+                    }else {
+                        $query = "UPDATE 'users' SET password = 
+                        '".md5(md5(mysqli_insert_id()). $_POST['password'])."'
+                        WHERE id = ".mysqli_insert_id($link)." LIMIT 1"; 
 
-                    mysqli_query($link, $query); 
+                        mysqli_query($link, $query); 
 
-                    // keep session if stay logged in is selected 
-                    $_SESSION['id'] = mysqli_insert_id($link); 
-                    if($_POST['stayLoggedIn'] == '1'){
-                        // we will save it for an hour for now. can do a year by multiple it all by 365
-                        setcookie("id", mysqli_insert_id($link), time()+ 60*60*24);
+                        // keep session if stay logged in is selected 
+                        $_SESSION['id'] = mysqli_insert_id($link); 
+                        if($_POST['stayLoggedIn'] == '1'){
+                            // we will save it for an hour for now. can do a year by multiple it all by 365
+                            setcookie("id", mysqli_insert_id($link), time()+ 60*60*24);
+                        }
+                        header("Location: loggedinPage.php"); 
+                        echo "sign up succcessful"; 
+
                     }
-                    header("Location: loggedinPage.php"); 
-                    echo "sign up succcessful"; 
+                }
+            } else {
 
-                 }
+                //working on login part now 
+                // doing a query to make sure they got user name and password correct 
+                $query = "SELECT id FROM 'users' WHERE 
+                email = '".mysqli_real_escape_string($link, $_POST['email']). "' ";
+                // run the query 
+                $result = mysqli_query($link, $query); 
+                $row = mysqli_fetch_array($result); 
+
+                // check to see if row exists 
+                if(isset($row)) {
+                    $hashedPassword = md5(md5($row['id']).$_POST['password']); 
+                    
+                    if($hashedPassword == $row['password']) {
+                        $_SESSION['id'] = $row['id']; 
+                        if($_POST['stayLoggedIn'] == '1') {
+                            setcookie("id", $row['id'], time()+ 60*60*24); 
+                        }
+                        header("Location: loggedinPage.php"); 
+                    }else {
+                        $error = "That email/password combo could not be found. Try again."; 
+                    }
+                }else {
+                    $error = "That email/password combo could not be found. Try again."; 
+                }
+
             }
         } 
     }
@@ -91,7 +120,17 @@
     <input type="email" name="email" placeholder="Enter your email">
     <input type="password" name="password" placeholder="Enter your password">
     <input type="checkbox" name="stayLoggedIn" value=1 >
+    <input type="hidden" name="signUp" value="1">
     <input type="submit" name="submit" value="Sign up!" >
+
+</form>
+
+<form method="post">
+    <input type="email" name="email" placeholder="Enter your email">
+    <input type="password" name="password" placeholder="Enter your password">
+    <input type="checkbox" name="stayLoggedIn" value=1 >
+    <input type="hidden" name="logIn" value="0">
+    <input type="submit" name="submit" value="Log in!!" >
 
 </form>
 
