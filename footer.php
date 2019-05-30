3c1a968ca6a2bb7f1dd334e895d17af8d0e7263b
Ahmad Hamed Rangeen <ahmadrangeen@Ahmads-MacBook-Pro.local>
2019-05-29 17:41:20 -0700

    added ajax to save contents of diary to database

--- footer.php
+++ footer.php
@@ -11,6 +11,20 @@
     $("#signUpform").toggle(); 
     $("#loginform").toggle(); 
     
+    $('#diary').bind('input propertychange', function() {
+
+        $.ajax({
+            method: "POST",
+            url:"updateDatabase.php",
+            data: {content: $("#diary").val()}
+        })
+        .done(function(msg){
+            alert("Data Saved: "+msg); 
+        }); 
+
+    }); 
+
+  
 
     </script> 
 
